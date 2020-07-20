2d9748db4f49c735e7b2a42e8fad392c534b334c
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-07-20 21:46:48 +0900

    rename to SEARCH_REPO

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,24 +1,24 @@
-import {gql} from '@apollo/client';
+import { gql } from '@apollo/client';
 
-export const SEARCHREPO = gql`
-    query getData($queryString: String!) {
-        search(query: $queryString, type: REPOSITORY, first: 10) {
-            nodes {
-                ... on Repository {
-                    databaseId
-                    nameWithOwner
-                    url
-                    createdAt
-                    description
-                    stargazers(orderBy:{field:STARRED_AT, direction:DESC}){
-                        totalCount
-                    }
-                    openGraphImageUrl
-                    primaryLanguage{
-                        name
-                    }
-                }
-            }
+export const SEARCH_REPO = gql`
+  query getData($queryString: String!) {
+    search(query: $queryString, type: REPOSITORY, first: 10) {
+      nodes {
+        ... on Repository {
+          databaseId
+          nameWithOwner
+          url
+          createdAt
+          description
+          stargazers(orderBy: { field: STARRED_AT, direction: DESC }) {
+            totalCount
+          }
+          openGraphImageUrl
+          primaryLanguage {
+            name
+          }
         }
+      }
     }
+  }
 `;
