4f4c8858ab9fb6d86ad25469fe5cc8e87d038d2c
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-07-21 15:46:49 +0900

    fix dependency

--- src/components/RepoInfo.jsx
+++ src/components/RepoInfo.jsx
@@ -1,84 +1,81 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import {useLazyQuery} from '@apollo/client';
-import styled from 'styled-components';
 import * as Graphql from '../graphql';
 
 const useInput = initialValue => {
-    const [value, set] = useState(initialValue)
-
-    return {value, onChange: (e) => set(e.target.value)}
+  const [value, set] = useState(initialValue)
+  
+  return {value, onChange: (e) => set(e.target.value)}
 }
 
-const useFetchData = () => {
-    const [getData, {loading, error, data}] = useLazyQuery(Graphql.SEARCHREPO)
-    const [query, setQuery] = useState('')
+const useRepoData = () => {
+  const [getData, {loading, error, data}] = useLazyQuery(Graphql.SEARCH_REPO)
+  const [query, setQuery] = useState('')
 
-    useEffect(() => {
-        getData({
-            variables: {
-                queryString: query
-            }
-        })
-    }, [getData, query])
-
-    return [setQuery, {loading, error, data}]
+  const fetchData = useCallback(() => {
+    getData({
+      variables: {
+        queryString: query
+      }
+    });
+  }, [getData, query]);
+  
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
+  
+  return [setQuery, {loading, error, data}]
 }
 
 const RepoInfo = () => {
-    const stars = useInput(0)
-    const language = useInput('')
-    const [fetchData, {loading, error, data}] = useFetchData()
-    
-    const handleOnClick = () => {
-        fetchData(`language:${language.value} stars:>${stars.value}`)
-    }
-    
-    if (loading) return <p>Loading Repository</p>
-    if (error) return <p>Error while searching Repository</p>
-
-    return (
-        <>
-            <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
-                <div className="navbar-brand">
-                    <a className='navbar-item'>
-                        GitRepo
-                    </a>
-                </div>
-            </nav>
-            <SearchContainer>
-                <label>
-                    Language:  
-                    <input type='text' {...language} />
-                </label>
-                <label>
-                    ★以上 
-                    <input type='text' {...stars} />
-                </label>
-
-                <button className="button is-small is-primary is-outlined" onClick={handleOnClick}>
-                    search
-                </button>
-            </SearchContainer>
-           
-            { data ? 
-            data.search.nodes.map(repo => 
-                <div key={repo.databaseId}>
-                    <h2>{repo.nameWithOwner}</h2>
-                    <img src={repo.openGraphImageUrl} alt='repoImage' />
-
-                </div>
-            )
-            : <></>
-            }
-        </>
-    )
+  const stars = useInput(0)
+  const language = useInput('')
+  const [fetchData, {loading, error, data}] = useRepoData()
+  
+  const handleOnClick = () => {
+    fetchData(`language:${language.value} stars:>${stars.value}`)
+  }
+  
+  if (loading) return <p>Loading Repository</p>
+  if (error) return <p>Error while searching Repository</p>
+  
+  return (
+    <>
+      <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <a className='navbar-item'>
+            GitRepo
+          </a>
+        </div>
+      </nav>
+      <label>
+        Language :
+        <input type='text' {...language} />
+      </label>
+      
+      <label>
+        More than : 
+        <input type='text' {...stars} />
+        Stars 
+      </label>
+      
+      <button className="button is-small is-primary is-outlined" onClick={handleOnClick}>
+        search
+      </button>
+      
+      
+      {
+        data ? data.search.nodes.map(
+          repo => 
+            <div key={repo.databaseId}>
+              <h2>{repo.nameWithOwner}</h2>
+              <img src={repo.openGraphImageUrl} alt='repoImage' />              
+            </div>
+        )
+          : <></>
+      }
+    </>
+  )
 }
-
-const SearchContainer = styled.div`
-    margin-top: 10px;
-    margin-right: auto;
-    margin-left: auto;
-    margin-bottom: 10px;
-`;
 
 export default RepoInfo;
